b8f63d6ed0487bc3ef7b89c7fc334159c1410497
Linus Nilsson <linus.erik.nilsson@outlook.com>
2022-05-25 15:55:13 +0200

    Added Stack spacing for FilterButton

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 
 function FilterButton(props) {
     return (
-        <Button
-            variant="contained"
-            type="button"
-            className="btn toggle-btn"
-            aria-pressed={props.isPressed}
-        >
-            <span className="visually-hidden">Show </span>
-            <span> {props.name}</span>
-            <span className="visually-hidden"> tasks</span>
 
-        </Button>
+        <Stack spacing={2} direction="row">
+        
+            <Button
+                variant="contained"
+                type="button"
+                className="btn toggle-btn"
+                aria-pressed={props.isPressed}
+                >
+                <span className="visually-hidden">Show </span>
+                <span> {props.name}</span>
+                <span className="visually-hidden"> tasks</span>
+
+            </Button>
+        </Stack>
 
     );
 }
